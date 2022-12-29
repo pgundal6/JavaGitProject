7f05ce399bcdbe29788606755f23e68994d6b13b
PriyankaGundal <priyanka.gundal.06@gmail.com>
2022-12-30 02:38:34 +0530

    First commit: Code

---
+++ src/com/gitdemo/GitDemo.java
@@ -0,0 +1,20 @@
+package com.gitdemo;
+
+import java.util.ArrayList;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Priyanka");
+		list.add("Angarki");
+		list.add("Akshay");
+
+		for (String s : list) {
+			System.out.println(s);
+		}
+
+
+	}
+
+}
